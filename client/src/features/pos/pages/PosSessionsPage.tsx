624617abac7924198c801c312f3c5a7203ea084b
Sahand Mohammed <108280493+SahandMohammed@users.noreply.github.com>
2026-09-21 15:38:31 +0300

    style(pos): widen create session dialog

--- client/src/features/pos/pages/PosSessionsPage.tsx
+++ client/src/features/pos/pages/PosSessionsPage.tsx
@@ -487,7 +487,7 @@ export function PosSessionsPage() {
       </Tabs>
 
       <Dialog open={openSessionDialog} onOpenChange={setOpenSessionDialog}>
-        <DialogContent className="max-h-[90vh] max-w-2xl overflow-y-auto p-0">
+        <DialogContent className="max-h-[90vh] w-[calc(100%-2rem)] max-w-4xl overflow-y-auto p-0 sm:w-full">
           {setup.data ? (
             <OpenSessionScreen
               embedded
